ca2759088e94271feadc0943fe3633949096f25f
Shogun <1790333@edu.vaniercollege.qc.ca>
2022-03-15 17:06:37 -0400

    Map Marker

--- MaptyApp/script.js
+++ MaptyApp/script.js
@@ -16,18 +16,26 @@ navigator.geolocation.getCurrentPosition(function(position) {
     const {latitude} = position.coords;
     const {longitude} = position.coords;
    
-
+    // computer coordinates
     const coords = [latitude, longitude];
 
     const map = L.map('map').setView(coords, 13);
 
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
 
-    L.marker(coords).addTo(map)
-    .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+    
+    // event created by leaflet
+    map.on('click',function(mapEvent) {
+        const {lat, lng} = mapEvent.latlng;
+        // marker placement and popup with methods from leaflet documentation
+    L.marker([lat,lng])
+    .addTo(map)
+    .bindPopup(L.popup({maxWidth: 250, minWidth: 100, autoClose: false, closeOnClick: false, className: 'running-popup'}))
+    .setPopupContent('Start a Workout') // Popup text content
     .openPopup();
+    });
 
 }, function(){
     alert('Could not get current position');
